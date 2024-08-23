8d088a679b389eaaf4e6bc997a884ebe0b88ecf1
Sumit-Badgujar <sumitbadgujar1000@gmail.com>
2024-08-23 23:58:20 +0530

    Added features :edit, view docs, + random card colors & shorter content preview in card

--- frontend-docs/src/components/Card.jsx
+++ frontend-docs/src/components/Card.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { LuDownload } from "react-icons/lu";
-import { IoCloseSharp } from "react-icons/io5";
+import React, { useState } from 'react'
+import { IoIosEye } from "react-icons/io";
 import { motion } from "framer-motion"
 import { MdEditNote } from "react-icons/md";
-import { MdDelete } from "react-icons/md";
 import { AiTwotoneDelete } from "react-icons/ai";
 import dayjs from 'dayjs';
 import axios from 'axios';
-
+import NotesForm from './NotesForm';
+import DocView from './DocView';
 
 function Card({ mainDataArr, data, setData, reference }) {
 
   const formatedDate = dayjs(data.updatedAt || data.createdAt).format('MMMM D, YYYY');
   const formatedTime = dayjs(data.updatedAt || data.createdAt).format('h:mm A');
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [isViewing, setIsViewing] = useState(false);
+
+  const showSmallContent = data.content.substring(0, 100) + '...';
+
+  const randomColor = Math.random() > 0.5 ? "bg-blue-600" : "bg-green-600";
+
   const deleteDoc = async () => {
     try {
       const response = await axios.delete(`http://localhost:3000/docs/v1/access/docs/delete/${data._id}`)
       if (response.status === 200) {
-        // const response = await axios.get('http://localhost:3000/docs/v1/access/docs/getall');
         setData(mainDataArr.filter(doc => doc._id !== data._id))
 
         console.log('Document successfully deleted');
@@ -29,46 +34,70 @@ function Card({ mainDataArr, data, setData, reference }) {
 
   };
   return (
-    <motion.div
-      drag
-      dragConstraints={reference}
-      whileDrag={{ scale: 1.1 }}
-      dragElastic={1}
-      dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }}
-      className=' relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8  overflow-hidden cursor-pointer '>
-      <MdEditNote className='text-2xl bg-slate-800 rounded-sm  ' />
-      <p className='text-sm leading-tight mt-5 font-thin overflow-clip '>{data.content}</p>
-
-      <div className='footer absolute bottom-0  w-full left-0'>
-
-        <div className='flex   items-center justify-between py-2  px-8 mb-3'>
-
-          <div className='text-xs font-thin text-sky-300 italic'>
-            <span>Updated at:</span>
-            <span>{formatedTime}</span>
-            <h3>{formatedDate}</h3>
+    <>
+      {
+        isEditing && (
+          <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[9999]'>
+            <NotesForm
+              onClose={() => setIsEditing(false)}
+              initialTitle={data.title}
+              initialContent={data.content}
+              docId={data._id}
+              setData={setData}
+              isEditing={isEditing}
+            />
           </div>
 
-          <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'
-            onClick={deleteDoc}
-          >
-            {/* {data.close ? <IoCloseSharp /> : <LuDownload  size='.7em' color='#fff'/> } */}
-            <AiTwotoneDelete />
-          </span>
+        )
+      }
+      {isViewing && (
+        <DocView data={data} formatedDate={formatedDate} formatedTime={formatedTime} setIsViewing={setIsViewing} />
+      )}
+      < motion.div
+        drag
+        dragConstraints={reference}
+        whileDrag={{ scale: 1.1 }
+        }
+        dragElastic={1}
+        dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }}
+        className=' relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8  overflow-hidden cursor-pointer ' >
+        <div className='flex justify-between'>
+          <MdEditNote className='text-2xl bg-slate-800 rounded-sm  ' onClick={() => setIsEditing(true)} />
+          <IoIosEye className='text-lg bg-slate-800 rounded-sm' onClick={() => setIsViewing(true)} />
+        </div>
+        <p className='text-sm leading-tight mt-5 font-thin overflow-clip '>{showSmallContent}</p>
+
+        <div className='footer absolute bottom-0  w-full left-0'>
+
+          <div className='flex   items-center justify-between py-2  px-8 mb-3'>
+
+            <div className='text-xs font-thin text-sky-300 italic'>
+              <span>Updated at:</span>
+              <span>{formatedTime}</span>
+              <h3>{formatedDate}</h3>
+            </div>
+
+            <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'
+              onClick={deleteDoc}
+            >
+              {/* {data.close ? <IoCloseSharp /> : <LuDownload  size='.7em' color='#fff'/> } */}
+              <AiTwotoneDelete />
+            </span>
+
+          </div>
+          {
+            <div className={`tag w-full py-4 ${randomColor}  `}>
+              <h3 className='text-sm flex items-center justify-center font-semibold'>{data.title}</h3>
+            </div>
+          }
+
 
         </div>
-        {
-          <div className={`tag w-full py-4 ${(data._id % 2) == 0 ? "bg-blue-600" : "bg-green-600"}  `}>
-            <h3 className='text-sm flex items-center justify-center font-semibold'>{data.title}</h3>
-          </div>
 
-        }
-
-
-      </div>
-
-    </motion.div>
+      </motion.div >
+    </>
   )
 }
 
 export default Card
+
